791f396427e2deb7e4dd2690143b5a5e1d4c068f
Ben Paget <ben.paget@live.fr>
2020-04-13 14:54:11 +0100

    initialized taskomatic app

--- src/index.js
+++ src/index.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
 import * as serviceWorker from './serviceWorker';
+import 'bootstrap/dist/css/bootstrap.css'
+import TaskApp from "./components/TaskApp"
+
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <TaskApp />
   </React.StrictMode>,
   document.getElementById('root')
 );
